200052725b654f52c8e2c152ed6b36a0a0af16d6
hlopeth <yaroslav.tolov@gmail.com>
2018-10-29 19:54:36 +0500

    Search v1

--- Angular/angular-training-simulator/src/app/app.module.ts
+++ Angular/angular-training-simulator/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { HeaderComponent } from './header/header.component';
 import { ProblemComponent } from './problem/problem.component';
+import { SertchComponent } from './sertch/sertch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriesComponent,
     HeaderComponent,
-    ProblemComponent
+    ProblemComponent,
+    SertchComponent
   ],
   imports: [
     BrowserModule
---
+++ Angular/angular-training-simulator/src/app/sertch/sertch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sertch',
+  templateUrl: './sertch.component.html',
+  styleUrls: ['./sertch.component.css']
+})
+export class SertchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
